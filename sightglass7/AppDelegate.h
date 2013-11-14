75986559ed8cfc90fda4c90ba415f6ca83fad999
Dave Kasper <dkasper@gmail.com>
2013-11-13 21:23:59 -0800

    Initial Commit

---
+++ sightglass7/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sightglass7
+//
+//  Created by David Kasper on 11/13/13.
+//  Copyright (c) 2013 David Kasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sightglass7Tests/sightglass7Tests.m
@@ -0,0 +1,34 @@
+//
+//  sightglass7Tests.m
+//  sightglass7Tests
+//
+//  Created by David Kasper on 11/13/13.
+//  Copyright (c) 2013 David Kasper. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface sightglass7Tests : XCTestCase
+
+@end
+
+@implementation sightglass7Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
